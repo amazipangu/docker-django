f34245ce14aeeded29f1aa4051c8715e006d2e0a
Shinya <s.pulscord@gmail.com>
2018-07-17 12:51:18 +0900

    :art: Fixed messages

--- cli/util.py
+++ cli/util.py
@@ -1,11 +1,7 @@
-import curses
-
-
 class Utility:
-    stdscr = curses.initscr()
 
     def input_service_name():
-        print("Input your service name which executed Django below.")
+        print("Type your service name which executed Django below.")
         service_name = input()
         print(
             "Your service name which has run Django: {}".format(service_name)
@@ -13,7 +9,7 @@ class Utility:
         return service_name
 
     def input_project_name():
-        print("Input your Django project name below.")
+        print("Type your Django project name below.")
         project_name = input()
         print(
             "your project name: {}".format(project_name)
